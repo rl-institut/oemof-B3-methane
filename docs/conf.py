4b35a73db5ef1f290df9ae670b4de0d8b058ab0c
Julius Meier <julius.meier@gmx.de>
2021-01-28 14:48:15 +0100

    Fix missing bibfile in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -36,6 +36,7 @@ extensions = [
     'sphinx.ext.napoleon',
     'sphinxcontrib.bibtex',
 ]
+bibtex_bibfiles = ['bibliography.bib']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
