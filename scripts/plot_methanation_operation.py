6f721a2b54523498c38675e6e7202aa4993d340d
monika <monika@orl25.de>
2022-05-10 10:42:00 +0200

    Add content to ax3 and ax4

--- scripts/plot_methanation_operation.py
+++ scripts/plot_methanation_operation.py
@@ -31,8 +31,32 @@ def load_results_sequences(directory):
 
     return sequences
 
+def load_storage_sequences(directory, file):
 
-def plot_methanation_operation(sequences_el, sequences_heat, methanation):
+    path = os.path.join(directory, file)
+
+    df = pd.read_csv(path, header=[0,1,2], parse_dates=[0], index_col=[0])
+
+    return df
+
+def filter_storage_sequences(df, region, bus, component):
+
+    columns = []
+    str = region + "-" + bus + "-" + component
+    for i in df.columns:
+        if i[0].startswith(str):
+            columns.append(i)
+    df = df[columns]
+
+    df = df.droplevel(["to", "type"], axis=1)
+    df.columns = df.columns.str.strip(region + "-")
+
+ #   df = plots.map_labels(df, labels_dict=labels_dict) # the labels are not found despite being in the yml file (?!)
+    return df
+
+
+def plot_methanation_operation(sequences_el, sequences_heat, sequences_methanation_reaction,
+                               sequences_methanation_storage):
 
     bus_name = ["B-electricity", "B-heat_central"]
 
@@ -61,6 +85,31 @@ def plot_methanation_operation(sequences_el, sequences_heat, methanation):
         for tick in ax.get_xticklabels():
             tick.set_rotation(45)
 
+    methanation = sequences_methanation_reaction[("B-h2-methanation", "B-h2-methanation-storage_products", "flow")]
+    ax3.fill_between(
+        methanation.index,
+        0,
+        methanation,
+        label="Methanation",
+#        color=colors_odict["Methanation"],
+    )
+
+    ax4.plot(
+        sequences_methanation_storage[("h2-methanation-storage_products")],
+        c="r",
+        label="Storage Level Products",
+    )
+    ax4.plot(
+        sequences_methanation_storage[("h2-methanation-storage_educts")],
+        c="b",
+        label="Storage Level Educts",
+    )
+
+
+ #   plots.plot_dispatch(ax4, sequences_methanation_storage, df_demand=pd.DataFrame(), unit="MWh")
+    # This shall replace the two above sections when labels and colors are being found.
+
+
     h_l = [ax.get_legend_handles_labels() for ax in (ax1, ax2, ax3, ax4)]
     handles = [item for sublist in list(map(lambda x: x[0], h_l)) for item in sublist]
     labels = [item for sublist in list(map(lambda x: x[1], h_l)) for item in sublist]
@@ -77,8 +126,8 @@ def plot_methanation_operation(sequences_el, sequences_heat, methanation):
 
     ax1.set_ylabel("Power")
     ax2.set_ylabel("Power")
-    ax3.set_ylabel("Storage level / MWh")
-    ax4.set_ylabel("Power / MW")
+    ax3.set_ylabel("Power / MW")
+    ax4.set_ylabel("Storage level / MWh")
     ax4.set_xlabel("Time")
 
     ax1.axes.get_xaxis().set_visible(False)
@@ -101,21 +150,28 @@ if __name__ == "__main__":
 
     component_directory = os.path.join(postprocessed, "sequences", "component")
 
+    variable_directory = os.path.join(postprocessed, "sequences", "by_variable")
+
     # create the directory plotted where all plots are saved
     if not os.path.exists(target):
         os.makedirs(target)
 
     bus_sequences = load_results_sequences(bus_directory)
 
-    methanation_sequences = load_results_sequences(component_directory)[
+    methanation_reaction_sequences = load_results_sequences(component_directory)[
         "methanation_reactor"
     ]
+    storage_sequences = load_storage_sequences(variable_directory, "storage_content.csv")
+
+    methanation_storage_sequences = filter_storage_sequences(storage_sequences, "B" , "h2", "methanation")
+
 
 
     plot_methanation_operation(
         bus_sequences["B-electricity"],
         bus_sequences["B-heat_central"],
-        methanation_sequences,
+        methanation_reaction_sequences,
+        methanation_storage_sequences,
     )
 
     plt.savefig(os.path.join(target, "methanation_operation.png"))
